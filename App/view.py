cf009d4552de15c1a93814717a6dc24a851f757f
l-villamil <l.villamil@uniandes.edu.co>
2020-11-11 17:31:56 -0500

    view acutualizada

--- App/view.py
+++ App/view.py
@@ -31,6 +31,9 @@ from App import controller
 from DISClib.ADT import stack
 import timeit
 assert config
+import config as cf
+
+
 
 """
 La vista se encarga de la interacción con el usuario.
@@ -42,12 +45,71 @@ operación seleccionada.
 # ___________________________________________________
 #  Variables
 # ___________________________________________________
-
-
+# servicefile="201801-1-citibike-tripdata.csv"
+# servicefile="201801-1-citibike-tripdata.csv"
+servicefile='201801-3-citibike-tripdata.csv'
 # ___________________________________________________
 #  Menu principal
 # ___________________________________________________
+def printMenu():
+    print("\n")
+    print("*******************************************")
+    print("Bienvenido")
+    print("1- Inicializar Analizador")
+    print("2- Cargar información de Citibike")
+    print("3- Calcular componentes conectados")
+
+def optionTwo():
+    print("\nCargando información de Citibike ....")
+    controller.loadFile(cont, servicefile)
+    # numedges = controller.totalConnections(cont)
+    # numvertex = controller.totalStops(cont)
+    # print('Numero de vertices: ' + str(numvertex))
+    # print('Numero de arcos: ' + str(numedges))
+    print('El limite de recursion actual: ' + str(sys.getrecursionlimit()))
+    sys.setrecursionlimit(recursionLimit)
+    print('El limite de recursion se ajusta a: ' + str(recursionLimit))
 
 """
 Menu principal
 """
+while True:
+    printMenu()
+    inputs = input('Seleccione una opción para continuar\n>')
+
+    if int(inputs[0]) == 1:
+        print("\nInicializando....")
+        # cont es el controlador que se usará de acá en adelante
+        cont = controller.init()
+
+    elif int(inputs[0]) == 2:
+        executiontime = timeit.timeit(optionTwo, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    elif int(inputs[0]) == 3:
+        executiontime = timeit.timeit(optionThree, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    elif int(inputs[0]) == 4:
+        msg = "Estación Base: BusStopCode-ServiceNo (Ej: 75009-10): "
+        initialStation = input(msg)
+        executiontime = timeit.timeit(optionFour, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    elif int(inputs[0]) == 5:
+        destStation = input("Estación destino (Ej: 15151-10): ")
+        executiontime = timeit.timeit(optionFive, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    elif int(inputs[0]) == 6:
+        destStation = input("Estación destino (Ej: 15151-10): ")
+        executiontime = timeit.timeit(optionSix, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    elif int(inputs[0]) == 7:
+        executiontime = timeit.timeit(optionSeven, number=1)
+        print("Tiempo de ejecución: " + str(executiontime))
+
+    else:
+        sys.exit(0)
+sys.exit(0)
